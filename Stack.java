ed9576d3480d616a5a0a9fe8ef31ff2caed94e6e
aa <aa>
2018-07-11 16:45:31 +0300

    java

--- Stack.java
+++ Stack.java
@@ -1,10 +1,39 @@
+import java.util.*;
+
 public class Stack {
-    //your code is here
-    public void push(int pushedElement){
-       //your code is here
-    }
-    public void pop() {
-        //your code is here
-    }
+/*
+public int counter=0;
+ArrayList<Integer> array = new ArrayList<Integer>(10);
+
+//your code is here
+public void push(int num){
+//your code is here
+
+array.add(num);
+counter++;
+
+}
+public void pop() {
+//your code is here
+array.remove(counter - 1);
+counter--;
+System.out.println("hello");
+}
+
+public static void main(String[] args){
+
+//stack tester
+Stack STACK1;
+STACK1 = new Stack();
+STACK1.push(23);
+STACK1.push(11);
+STACK1.push(2);
+System.out.println(STACK1.array);
+STACK1.pop();
+STACK1.pop();
+
+System.out.println(STACK1.array);
+}
+*/
 
 }
